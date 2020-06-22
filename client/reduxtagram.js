47e18de7d3571ae1636eaa48d8aafc4b8f6ba539
Jaehyuk Lee <jhlumd@gmail.com>
2020-06-21 22:18:10 -0700

    Create main component and render app

--- client/reduxtagram.js
+++ client/reduxtagram.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
+
+// Import styles
 import css from "./styles/style.styl";
 
-render(<p>hello</p>, document.getElementById("root"));
+// Import Components
+import Main from "./components/main";
+
+render(<Main />, document.getElementById("root"));
