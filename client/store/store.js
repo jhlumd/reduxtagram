1d178c9d7257bfc1aa878851b19c684c6ccf3b4e
Jaehyuk Lee <jhlumd@gmail.com>
2020-07-04 18:10:47 -0700

    Add config for redux devtools

--- client/store/store.js
+++ client/store/store.js
@@ -1,4 +1,4 @@
-import { createStore, compose } from "redux";
+import { createStore } from "redux";
 import { syncHistoryWithStore } from "react-router-redux";
 import { browserHistory } from "react-router";
 
@@ -9,17 +9,21 @@ import rootReducer from "../reducers/root_reducer";
 import posts from "../data/posts";
 import comments from "../data/comments";
 
-const stateWithSeed = { posts, comments };
-const store = createStore(rootReducer, stateWithSeed);
+const preloadedState = { posts, comments };
+const store = createStore(
+  rootReducer,
+  preloadedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
-// For hot reloading reducers with webpack
-if (module.hot) {
-  module.hot.accept("../reducers/root_reducer", () => {
-    const newRootReducer = require("../reducers/root_reducer").default;
-    store.replaceReducer(newRootReducer);
-  });
-}
+// // For hot reloading reducers with webpack
+// if (module.hot) {
+//   module.hot.accept("../reducers/root_reducer", () => {
+//     const newRootReducer = require("../reducers/root_reducer").default;
+//     store.replaceReducer(newRootReducer);
+//   });
+// }
 
 export default store;
